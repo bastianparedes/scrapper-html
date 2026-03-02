c90af88b0abb1381b82c75aec7a1c51c61fe25b3
Bastián <bparedesp@falabella.cl>
2026-03-02 16:05:44 -0300

    done

--- paso3-colectar-data.js
+++ paso3-colectar-data.js
@@ -36,8 +36,7 @@ async function main() {
 
     for (let url of urls) {
       await driver.get(url);
-      const title = await driver.getTitle() || "";
-
+      
       const description = await driver.executeScript(
         "return document.querySelector('meta[name=\"description\"]')?.content || '';"
       );
@@ -45,17 +44,32 @@ async function main() {
       const canonicalUrl = await driver.executeScript(
         'return document.querySelector("link[rel=\\"canonical\\"]")?.href || "";'
       );
-
-      const promotionalText = await driver.executeScript(
+      const canonical = canonicalUrl || '';
+      
+      const rawPromotionalText = await driver.executeScript(
         'return document.querySelector(".promotional-content-container")?.innerText || "";'
       );
 
-      arrayData.push({
-        title,
-        description,
-        canonicalUrl,
-        promotionalText
-      });
+      const typePage = canonical.replace('https://', '').split('/')[2];
+      const pageId = canonical.replace('https://', '').split('/')[3];
+
+      let promotionalText = rawPromotionalText || "";
+      promotionalText = promotionalText.charAt(0).toUpperCase() + promotionalText.slice(1);
+      const promotionalTextArray = promotionalText.split("\n");
+      promotionalTextArray[0] = `<h2>${promotionalTextArray[0]}</h2>`;
+      promotionalText = promotionalTextArray.join("<br>").replace('</h2><br><br>', '</h2>');
+
+
+      const object = {};
+      if (['category', 'collection', 'brand', 'seller'].includes(typePage)) object.pageId = pageId;
+      if (['shop'].includes(typePage)) object.searchTerm = pageId;
+      object.title = await driver.getTitle() || "";
+      object.description = description || '';
+      object.canonical = canonical || '';
+      object.promotionalText = promotionalText || '';
+      object.delete = false;
+
+      arrayData.push(object);
     }
     await appendFile(jsonFileName, JSON.stringify(arrayData, null, 2));
 
--- paso4-json-a-csv.js
+++ paso4-json-a-csv.js
@@ -11,14 +11,7 @@ const rawData = fs.readFileSync(inputFile, "utf8");
 const data = JSON.parse(rawData);
 
 // Headers CSV
-const headers = [
-  "searchTerm",
-  "title",
-  "description",
-  "canonical",
-  "promotionalText",
-  "delete"
-];
+const headers = Object.keys(data[0]);
 
 // Función para escapar valores CSV
 const escapeCsv = (value) => {
@@ -32,21 +25,12 @@ const escapeCsv = (value) => {
 
 // Procesar filas
 const rows = data.map(item => {
-  const canonical = item.canonicalUrl || "";
-  const searchTerm = canonical.split("/").filter(Boolean).pop() || "";
-
-  let promotionalText = item.promotionalText || "";
-  promotionalText = promotionalText.charAt(0).toUpperCase() + promotionalText.slice(1);
-  const promotionalTextArray = promotionalText.split("\n");
-  promotionalTextArray[0] = `<h2>${promotionalTextArray[0]}</h2>`;
-  promotionalText = promotionalTextArray.join("<br>").replace('</h2><br><br>', '</h2>');
-
   return [
-    searchTerm,
+    item.pageId,
     item.title || "",
     item.description || "",
-    canonical,
-    promotionalText,
+    item.canonical,
+    item.promotionalText,
     'false'
   ].map(escapeCsv).join(",");
 });
